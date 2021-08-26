f1c693ca9ce5ea99b59b7190374bcebf9c87f7a6
Jacqueline Yeung <j.yeung@salesforce.com>
2021-08-26 15:39:17 -0700

    basic streaming to silver table now works! Added deletion of warehouse and checkpoint folders

--- src/main/java/IcebergNormalizationTest.java
+++ src/main/java/IcebergNormalizationTest.java
@@ -1,12 +1,14 @@
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.hadoop.HadoopCatalog;
+import org.apache.iceberg.hadoop.HadoopTables;
 import org.apache.iceberg.types.Types;
-import org.apache.logging.log4j.simple.SimpleLogger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.ForeachWriter;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.streaming.Trigger;
@@ -14,22 +16,18 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.apache.iceberg.*;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import javax.validation.constraints.AssertTrue;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-
-import static org.apache.spark.sql.functions.col;
-
 
 /*
 Create initial source Bronze table.
@@ -47,11 +45,9 @@ public class IcebergNormalizationTest {
     private static Table bronzeTable;
     private static Table silverTable1;
     private static Table silverTable2;
-    private static Catalog bronzeCatalog;
+    private static Catalog catalog;
     private static TableIdentifier bronzeTableId;
-    private static Catalog silverCatalog1;
     private static TableIdentifier silverTableId1;
-    private static Catalog silverCatalog2;
     private static TableIdentifier silverTableId2;
 
     private static final String WAREHOUSE = "warehouse";
@@ -59,14 +55,14 @@ public class IcebergNormalizationTest {
     private static final String BRONZE_NAMESPACE = "bronze_namespace";
     private static final String BRONZE_TABLE_NAME = "bronze_table";
     private static final String BRONZE_SQL_TABLE = CATALOG + "." + BRONZE_NAMESPACE + "." + BRONZE_TABLE_NAME;
-    private static final String BRONZE_TABLE_PATH = WAREHOUSE + "." + BRONZE_SQL_TABLE;
+    private static final String BRONZE_TABLE_PATH = "warehouse/bronze_namespace/bronze_table";
     private static final String SILVER_NAMESPACE = "silver_namespace";
     private static final String SILVER_TABLE_NAME1 = "silver_table1";
     private static final String SILVER_TABLE_NAME2 = "silver_table2";
     private static final String SILVER_SQL_TABLE1 = CATALOG + "." + SILVER_NAMESPACE + "." + SILVER_TABLE_NAME1;
     private static final String SILVER_SQL_TABLE2 = CATALOG + "." + SILVER_NAMESPACE + "." + SILVER_TABLE_NAME2;
-    private static final String SILVER_TABLE_PATH1 = WAREHOUSE + "." + SILVER_SQL_TABLE1;
-    private static final String SILVER_TABLE_PATH2 = WAREHOUSE + "." + SILVER_SQL_TABLE2;
+    private static final String SILVER_TABLE_PATH1 = "warehouse/silver_namespace/silver_table1";
+    private static final String SILVER_TABLE_PATH2 = "warehouse/silver_namespace/silver_table2";
 
     private static Dataset<Row> bronzeSourceStreamDf;
     private static Dataset<Row> silverSinkStreamDf1;
@@ -79,62 +75,47 @@ public class IcebergNormalizationTest {
     Create streaming job.
      */
     @BeforeClass
-    public static void setup() {
+    public static void setup() throws NoSuchTableException {
         setSparkConf();
         setSparkSession();
         createBronzeTable();
         createSilverTables();
 
+        spark.sql("INSERT INTO " + BRONZE_SQL_TABLE + " VALUES " +
+                "(1, \'abc\', \'bcd\', 123, \'redmond\', 98022, \'usa\', 343, \'bellevue\', 98077, \'usa\', current_timestamp())," +
+                "(2, \'some\', \'one\', 444, \'seattle\', 98008, \'usa\', NULL, NULL, NULL, NULL, current_timestamp())");
+
+        spark.table(BRONZE_SQL_TABLE).show();
         bronzeSourceStreamDf = spark.readStream()
                 .format("iceberg")
                 .table(BRONZE_SQL_TABLE);
 
-        assertTrue(bronzeSourceStreamDf.isStreaming());
-
         silverSinkStreamDf1 = bronzeSourceStreamDf.select("id", "firstName", "lastName");
 
-
-//        var writer = new ForeachWriter<Row>() {
-//            @Override
-//            public boolean open(long partitionId, long epochId) {
-//                return true;
-//            }
-//
-//            @Override
-//            public void process(Row value) {
-//
-//            }
-//
-//            @Override
-//            public void close(Throwable errorOrNull) {
-//
-//            }
-//        }
+        assertTrue(silverSinkStreamDf1.isStreaming());
 
         try {
+//            query = silverSinkStreamDf1.writeStream()
+//                    .format("console")
+//                    .outputMode("append")
+//                    .start();
+
             query = silverSinkStreamDf1.writeStream()
-//                    .foreach()
                     .format("iceberg")
                     .outputMode("append")
-//                    .option("path", SILVER_SQL_TABLE1)
-                    .trigger(Trigger.ProcessingTime(1, TimeUnit.MILLISECONDS))
+                    .option("path", SILVER_SQL_TABLE1)
                     .option("checkpointLocation", SILVER_TABLE_NAME1 + "_checkpoint")
-                    .toTable(SILVER_SQL_TABLE1);
-        } catch (TimeoutException e) {
-            System.out.println("PRINTING OUT ERROR STACK TRACE-----------------------------------------");
+                    .start();
+
+
+            Thread.sleep(2000);
+            assertTrue(query.isActive());
+//            query.stop();
+        } catch (TimeoutException | InterruptedException e) {
             e.printStackTrace();
-            System.out.println("PRINTING OUT ERROR STACK TRACE-----------------------------------------");
             tearDown();
         }
     }
-
-    /*
-    For-each row processing function
-     */
-    public void processRow(Dataset<String> dataset, Long batchId) {
-
-    }
-
 
     /*
     Delete all tables.
@@ -146,12 +127,16 @@ public class IcebergNormalizationTest {
         } catch (TimeoutException e) {
             e.printStackTrace();
         }
-        bronzeCatalog.dropTable(bronzeTableId);
-        silverCatalog1.dropTable(silverTableId1);
-        silverCatalog2.dropTable(silverTableId2);
         spark.sql("DROP TABLE IF EXISTS " + BRONZE_SQL_TABLE);
         spark.sql("DROP TABLE IF EXISTS " + SILVER_SQL_TABLE1);
         spark.sql("DROP TABLE IF EXISTS " + SILVER_SQL_TABLE2);
+        // delete checkpts
+        try {
+            FileUtils.deleteDirectory(FileUtils.getFile(WAREHOUSE));
+            FileUtils.deleteDirectory(FileUtils.getFile(SILVER_TABLE_NAME1 + "_checkpoint"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void setSparkSession() {
@@ -161,20 +146,11 @@ public class IcebergNormalizationTest {
                 .master(CATALOG)
                 .config(sparkConf)
                 .getOrCreate();
-        sparkContext = new JavaSparkContext(spark.sparkContext());
 
     }
 
     private static void setSparkConf() {
         sparkConf = new SparkConf();
-        // spark master URL for distributed cluster: run locally with 1 thread
-//        sparkConf.set("spark.master", CATALOG);
-
-        // spark catalog: for non-iceberg tables
-//        sparkConf.set("spark.sql.extensions", "org.apache.initialceberg.spark.extensions.IcebergSparkSessionExtensions");
-//        sparkConf.set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog");
-//        sparkConf.set("spark.sql.catalog.spark_catalog.type", "hive");
-
         // local catalog: directory-based in HDFS, for iceberg tables
         sparkConf.set("spark.sql.catalog." + CATALOG, "org.apache.iceberg.spark.SparkCatalog");
         sparkConf.set("spark.sql.catalog." + CATALOG + ".type", "hadoop");
@@ -202,18 +178,13 @@ public class IcebergNormalizationTest {
                 .build();
 
         // Catalog method of creating Iceberg table
-        bronzeCatalog = new HadoopCatalog(new Configuration(), WAREHOUSE);
-        bronzeTableId = TableIdentifier.of(BRONZE_NAMESPACE, BRONZE_TABLE_NAME);
-        bronzeTable = bronzeCatalog.createTable(bronzeTableId, bronzeSchema, bronzeSpec);
+        bronzeTable = createOrReplaceHadoopTable(bronzeSchema, bronzeSpec, new HashMap<>(), BRONZE_TABLE_PATH);
 
-        bronzeSparkDataset = spark.sql("CREATE TABLE IF NOT EXISTS " + BRONZE_SQL_TABLE +
+        spark.sql("CREATE TABLE IF NOT EXISTS " + BRONZE_SQL_TABLE +
                 "(id bigint, firstName string, lastName string," +
                 "streetNo1 int, cityName1 string, zipcode1 int, county1 string," +
                 "streetNo2 int, cityName2 string, zipcode2 int, county2 string, arrivalTime timestamp) " +
                 "USING iceberg");
-
-//         Table interface method of creating Iceberg table
-//        bronzeTable = new HadoopTables().create(bronzeSchema, BRONZE_NAMESPACE + "." + BRONZE_TABLE_NAME);
     }
 
     private static void createSilverTables() {
@@ -244,22 +215,23 @@ public class IcebergNormalizationTest {
                 .build();
 
         // Catalog method of creating Iceberg table
-        silverCatalog1 = new HadoopCatalog(new Configuration(), WAREHOUSE);
-        silverTableId1 = TableIdentifier.of(SILVER_NAMESPACE, SILVER_TABLE_NAME1);
-        silverTable1 = silverCatalog1.createTable(silverTableId1, silverSchema1, silverSpec1);
-        silverCatalog2 = new HadoopCatalog(new Configuration(), WAREHOUSE);
-        silverTableId2 = TableIdentifier.of(SILVER_NAMESPACE, SILVER_TABLE_NAME2);
-        silverTable2 = silverCatalog2.createTable(silverTableId2, silverSchema2, silverSpec2);
+        silverTable1 = createOrReplaceHadoopTable(silverSchema1, silverSpec1, new HashMap<>(), SILVER_TABLE_PATH1);
+        silverTable2 = createOrReplaceHadoopTable(silverSchema2, silverSpec2, new HashMap<>(), SILVER_TABLE_PATH2);
 
-        silverSparkDataset1 = spark.sql("CREATE TABLE IF NOT EXISTS " + SILVER_SQL_TABLE1 +
+        spark.sql("CREATE TABLE IF NOT EXISTS " + SILVER_SQL_TABLE1 +
                 "(id bigint, firstName string, lastName string) " +
                 "USING iceberg");
 
-        silverSparkDataset2 = spark.sql("CREATE TABLE IF NOT EXISTS " + SILVER_SQL_TABLE2 +
+        spark.sql("CREATE TABLE IF NOT EXISTS " + SILVER_SQL_TABLE2 +
                 "(AddressId bigint, PartyId bigint, " +
                 "streetNo int, cityName string, zipcode int, county string) " +
                 "USING iceberg");
+    }
 
+    private static Table createOrReplaceHadoopTable(Schema schema, PartitionSpec spec, Map<String, String> properties, String tableIdentifier) {
+        HadoopTables tables = new HadoopTables(spark.sparkContext().hadoopConfiguration());
+        tables.dropTable(tableIdentifier);
+        return tables.create(schema, spec, properties, tableIdentifier);
     }
 
     /*
@@ -267,17 +239,16 @@ public class IcebergNormalizationTest {
      */
 
     @Test
-    public void addEntireRecordTest() {
-        bronzeSparkDataset = spark.sql("INSERT INTO " + BRONZE_SQL_TABLE + " VALUES " +
-                "(1, \'abc\', \'bcd\', 123, \'redmond\', 98022, \'usa\', 343, \'bellevue\', 98077, \'usa\', current_timestamp())," +
-                "(2, \'some\', \'one\', 444, \'seattle\', 98008, \'usa\', NULL, NULL, NULL, NULL, current_timestamp())");
+    public void addEntireRecordTest() throws InterruptedException {
 
-        spark.read().table(SILVER_SQL_TABLE1).show();
+        spark.read().format("iceberg").table(SILVER_SQL_TABLE1).show();
 
         spark.sql("INSERT INTO " + BRONZE_SQL_TABLE + " VALUES " +
                 "(3, \'no\', \'one\', 456, \'boston\', 90578, \'usa\', 888, \'san francisco\', 99999, \'usa\', current_timestamp())");
 
-        spark.read().table(SILVER_SQL_TABLE1).show();
+        Thread.sleep(2000);
+
+        spark.read().format("iceberg").table(SILVER_SQL_TABLE1).show();
     }
 
 }
